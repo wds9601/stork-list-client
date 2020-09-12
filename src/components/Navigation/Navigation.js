c5379509907a0f9c393568ba6e503fffd2cf3f33
William Downey-Schell <Will@Williams-MacBook-Air.local>
2020-09-12 00:30:06 -0700

    setup eslint, prettier, created Nav component

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navigation = () => {
+	return (
+		<nav style={{ display: 'flex', justifyContent: 'flex-start' }}>
+			<p>Logo Here</p>
+			<p>Log In</p>
+			<p>Sign Up</p>
+		</nav>
+	);
+};
+
+export default Navigation;
